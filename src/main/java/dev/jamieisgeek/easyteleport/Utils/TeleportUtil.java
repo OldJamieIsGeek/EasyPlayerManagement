41a5ec4791ceb002933748be926876e14993e1af
Jamie <jamie@sootmc.net>
2023-01-11 18:48:45 +0000

    Add early return and better memory usage

--- src/main/java/dev/jamieisgeek/easyteleport/Utils/TeleportUtil.java
+++ src/main/java/dev/jamieisgeek/easyteleport/Utils/TeleportUtil.java
@@ -9,18 +9,17 @@ public class TeleportUtil {
 
     public static void TeleportToPlayer(InventoryClickEvent e, Player p, Player tpPlayer) {
 
-        Location tpLocation = tpPlayer.getLocation();
-
         if(p.hasPermission("etp.tp")) {
-
-            p.sendMessage(ChatColor.GREEN + "Teleporting...");
-            p.teleport(tpLocation);
-            p.closeInventory();
-            p.sendMessage(ChatColor.GREEN + "Teleported you to: " + ChatColor.RED + tpPlayer.getDisplayName() + ChatColor.GREEN + "'s Location!");
-        } else {
             p.closeInventory();
             p.sendMessage(ChatColor.RED + "Error: Missing Permission" + ChatColor.WHITE + "etp.tp");
+            return;
         }
+
+        p.sendMessage(ChatColor.GREEN + "Teleporting...");
+        p.teleport(tpPlayer.getLocation());
+        p.closeInventory();
+        p.sendMessage(ChatColor.GREEN + "Teleported you to: " + ChatColor.RED + tpPlayer.getDisplayName() + ChatColor.GREEN + "'s Location!");
+       
     }
 
 }
